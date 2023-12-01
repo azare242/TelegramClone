a7b004e85ea911f793409e5bf3be1b7adc8ff9f8
Alireza Zare <azare242@gmail.com>
2023-12-02 00:10:55 +0330

    lalalala

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
 import {Route, Routes, useParams} from 'react-router-dom'
+import {Button} from '@mui/material'
+import React from 'react';
 
 
 function Child() {
   const {id} = useParams();
+  const [underline, setUnderline] = React.useState<string>('underline')
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Hello world! {id}</h1>
+    <h1 className={`text-3xl font-bold ${underline}`}>Hello world! {id}</h1>
+    <Button onClick={() => {
+      if (underline === "underline") setUnderline("")
+      else setUnderline("underline");
+    }}
+    variant='contained'
+    >CLICK ME</Button>
     </>
   )
 } 
@@ -15,6 +24,7 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center" >
       <Routes>
+
         <Route path='/:id' element={<Child/>}/>
       </Routes>
       
