741a84c32d2fbf9a1277486d147b283b7fcf46ab
s.mohamadi <s.mohamadi@simra.cloud>
2023-12-23 18:39:35 +0330

    add group chat and contact impl

--- backend/internal/infra/repository/contactPostgres/contact.go
+++ backend/internal/infra/repository/contactPostgres/contact.go
@@ -2,6 +2,8 @@ package contactPostgres
 
 import (
 	"backend/internal/domain/model"
+	"backend/internal/domain/repository/contactRepo"
+	"context"
 	"gorm.io/gorm"
 	"time"
 )
@@ -31,7 +33,7 @@ func (c *ContactDTO) ToContact() *model.Contact {
 	}
 }
 
-func ToContactDTO(contact *model.Contact) *ContactDTO {
+func ToContactDTO(contact model.Contact) *ContactDTO {
 	return &ContactDTO{
 		Contact: model.Contact{
 			ContactID:     contact.ContactID,
@@ -40,6 +42,82 @@ func ToContactDTO(contact *model.Contact) *ContactDTO {
 			Status:        contact.Status,
 		},
 		CreatedAt: time.Now(),
+	}
+}
+
+func (c *Repository) Create(ctx context.Context, contact model.Contact) error {
+	contactDTO := ToContactDTO(contact)
+
+	result := c.db.WithContext(ctx).Create(contactDTO)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (c *Repository) Get(ctx context.Context, cmd contactRepo.GetCommand) ([]model.Contact, error) {
+	var contactDTOs []ContactDTO
+	var condition ContactDTO
+
+	if cmd.ID != nil {
+		condition.ContactID = *cmd.ID
+	}
+	if cmd.UserID != nil {
+		condition.UserID = *cmd.UserID
+	}
+	if cmd.Status != nil {
+		condition.Status = *cmd.Status
+	}
+
+
+	result := c.db.WithContext(ctx).Where(&condition).Find(&contactDTOs)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	contacts := make([]model.Contact, len(contactDTOs))
+	for i, contactDTO := range contactDTOs {
+		contacts[i] = *contactDTO.ToContact()
+	}
+
+	return contacts, nil
+}
+
+func (c *Repository) Update(ctx context.Context, contact model.Contact) error {
+	var condition ContactDTO
+	condition.ContactID = contact.ContactID
+
+	dto := ContactDTO{
+		Contact:      contact,
 		UpdatedAt: time.Now(),
 	}
+
+	result := c.db.WithContext(ctx).Where(&condition).Updates(&dto)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (c *Repository) Delete(ctx context.Context, cmd contactRepo.GetCommand) error {
+	var condition ContactDTO
+
+	if cmd.ID != nil {
+		condition.ContactID = *cmd.ID
+	}
+	if cmd.UserID != nil {
+		condition.UserID = *cmd.UserID
+	}
+	if cmd.Status != nil {
+		condition.Status = *cmd.Status
+	}
+
+	result := c.db.WithContext(ctx).Where(&condition).Delete(&ContactDTO{})
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
--- backend/internal/infra/repository/groupChatPostgres/groupChat.go
+++ backend/internal/infra/repository/groupChatPostgres/groupChat.go
@@ -2,6 +2,8 @@ package groupChatPostgres
 
 import (
 	"backend/internal/domain/model"
+	"backend/internal/domain/repository/groupChatRepo"
+	"context"
 	"gorm.io/gorm"
 	"time"
 )
@@ -30,7 +32,7 @@ func (u *GroupChatDTO) ToGroupChat() *model.GroupChat {
 	}
 }
 
-func ToGroupChatDTO(groupChat *model.GroupChat) *GroupChatDTO {
+func ToGroupChatDTO(groupChat model.GroupChat) *GroupChatDTO {
 	return &GroupChatDTO{
 		GroupChat: model.GroupChat{
 			GroupChatID: groupChat.GroupChatID,
@@ -38,6 +40,75 @@ func ToGroupChatDTO(groupChat *model.GroupChat) *GroupChatDTO {
 			MessageID:   groupChat.MessageID,
 		},
 		CreatedAt: time.Now(),
+	}
+}
+
+func (u *Repository) Create(ctx context.Context, groupChat model.GroupChat) error {
+	groupChatDTO := ToGroupChatDTO(groupChat)
+
+	result := u.db.WithContext(ctx).Create(groupChatDTO)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (u *Repository) Get(ctx context.Context, cmd groupChatRepo.GetCommand) ([]model.GroupChat, error) {
+	var GroupChatDTOs []GroupChatDTO
+	var condition GroupChatDTO
+
+	if cmd.ID != nil {
+		condition.GroupChatID = *cmd.ID
+	}
+	if cmd.GroupID != nil {
+		condition.GroupID = *cmd.GroupID
+	}
+
+	result := u.db.WithContext(ctx).Where(&condition).Find(&GroupChatDTOs)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	groupChats := make([]model.GroupChat, len(GroupChatDTOs))
+	for i, userDTO := range GroupChatDTOs {
+		groupChats[i] = *userDTO.ToGroupChat()
+	}
+
+	return groupChats, nil
+}
+
+func (u *Repository) Update(ctx context.Context, groupChat model.GroupChat) error {
+	var condition GroupChatDTO
+	condition.GroupChatID = groupChat.GroupChatID
+
+	dto := GroupChatDTO{
+		GroupChat: groupChat,
 		UpdatedAt: time.Now(),
 	}
+
+	result := u.db.WithContext(ctx).Where(&condition).Updates(&dto)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (u *Repository) Delete(ctx context.Context, cmd groupChatRepo.GetCommand) error {
+	var condition GroupChatDTO
+
+	if cmd.ID != nil {
+		condition.GroupChatID = *cmd.ID
+	}
+	if cmd.GroupID != nil {
+		condition.GroupID = *cmd.GroupID
+	}
+
+	result := u.db.WithContext(ctx).Where(&condition).Delete(&GroupChatDTO{})
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
