656c7c0a0fb4f8298a142696032925a771317ce7
mohamad sadegh mohamadi <msmohamadi1380@gmail.com>
2024-01-30 09:16:24 +0330

    fix database bugs

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"backend/internal/domain/model"
 	"backend/internal/infra/config"
 	"backend/internal/infra/http/handler"
 	"backend/internal/infra/http/middleware"
+	"backend/internal/infra/repository/contactPostgres"
+	"backend/internal/infra/repository/groupChatPostgres"
+	"backend/internal/infra/repository/groupPostgres"
+	"backend/internal/infra/repository/messagePostgres"
+	"backend/internal/infra/repository/userChatPostgres"
+	"backend/internal/infra/repository/userGroupPostgres"
 	"backend/internal/infra/repository/userPostgres"
 	"strconv"
 
@@ -15,7 +20,22 @@ import (
 )
 
 func main() {
-	db := &gorm.DB{}
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Warnln("cant find config file")
+	}
+
+	db, err := gorm.Open(sqlite.Open("messenger.db"), new(gorm.Config))
+	if err != nil {
+		log.Warnln("cant open db")
+		return
+	}
+
+	err = db.AutoMigrate(new(userPostgres.UserDTO), new(userChatPostgres.UserChatDTO), new(messagePostgres.MessageDTO), new(groupPostgres.GroupDTO),
+		new(contactPostgres.ContactDTO), new(userGroupPostgres.UserGroupDTO), new(groupChatPostgres.GroupChatDTO))
+	if err != nil {
+		return
+	}
 
 	repou := userPostgres.New(db)
 
@@ -26,16 +46,6 @@ func main() {
 	//repogc := groupChatPostgres.New(db)
 	//repouc := userChatPostgres.New(db)
 
-	var err error
-	db, err = gorm.Open(sqlite.Open("messenger.db"), new(gorm.Config))
-	if err != nil {
-		log.Warnln("cant open db")
-		return
-	}
-	conf, err := config.LoadConfig()
-	if err != nil {
-		log.Warnln("cant find config file")
-	}
 	e := echo.New()
 
 	e.Use(middleware.CustomLoggerMiddleware)
@@ -54,11 +64,6 @@ func main() {
 	hu.NewUserHandler(e.Group("/api"))
 
 	//hg := handler.NewGroup(repog)
-
-	err = db.AutoMigrate(&model.User{}, &model.Chat{}, &model.Message{}, &model.Group{}, &model.Contact{}, &model.UserGroup{}, &model.GroupChat{})
-	if err != nil {
-		return
-	}
 
 	if err := e.Start(conf.Server.Address + ":" + strconv.Itoa(conf.Server.Port)); err != nil {
 		log.Fatalf("server failed to start %v", err)
