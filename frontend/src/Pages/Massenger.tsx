a829ee1ab5f2b87c6005eeb68e3a10b715b5c19d
azare242 <azare242@gmail.com>
2024-02-01 18:03:20 +0330

    feat(front): group chat page

--- frontend/src/Pages/Massenger.tsx
+++ frontend/src/Pages/Massenger.tsx
@@ -4,8 +4,8 @@ import MassengerLayout from '../Layouts/Massenger';
 import { useAPI } from '../Actions/API/useAPI';
 import LoadingInPage from '../Components/Loading/LoadingInPage';
 import { useNavigate, useParams } from 'react-router-dom';
-import { ChatPagePV } from '../Components/Chat/ChatPage';
-import { OtherUserMessage, UserMessage } from '../Components/Message/Message';
+import { ChatPageGroup, ChatPagePV } from '../Components/Chat/ChatPage';
+import { OtherUserMessage, OtherUserMessageInGroup, UserMessage } from '../Components/Message/Message';
 
 const Massenger: React.FC<{ openedChat: boolean }> = ({ openedChat }) => {
   const { getChats, getGroups } = useAPI();
@@ -18,7 +18,7 @@ const Massenger: React.FC<{ openedChat: boolean }> = ({ openedChat }) => {
       navigate('/ppllss');
     }
 
-    const api = chattype === 'pv' ? getChats : getGroups;
+    const api = chattype === 'pv' ? getChats : getChats;
     async function fetch() {
       const res =
         api === null
@@ -50,7 +50,7 @@ const Massenger: React.FC<{ openedChat: boolean }> = ({ openedChat }) => {
         <LoadingInPage />
       )}
       {!openedChat && <div className='container mx-auto p-4 h-full'></div>}
-      {openedChat && <div className='container mx-auto p-4 h-full'><ChatPagePV userImage='' userName='alireza'>
+      {openedChat && chattype === 'pv' && <div className='container mx-auto p-4 h-full'><ChatPagePV userImage='' userName='alireza'>
         <UserMessage text="hi"></UserMessage>
         <UserMessage text="hi"></UserMessage>
 
@@ -68,6 +68,23 @@ const Massenger: React.FC<{ openedChat: boolean }> = ({ openedChat }) => {
         <OtherUserMessage text='hello'></OtherUserMessage>
         
         </ChatPagePV></div>}
+        {openedChat && chattype === 'group' && <div className='container mx-auto p-4 h-full'><ChatPageGroup gpImage='' gpName='Group'>
+        <UserMessage text="hi"></UserMessage>
+        <UserMessage text="hi"></UserMessage>
+
+        <UserMessage text="hi"></UserMessage>
+        <UserMessage text="hi"></UserMessage>
+        <UserMessage text="hi"></UserMessage>
+        <OtherUserMessageInGroup text='hello' username='alireza' avatarSrc=''></OtherUserMessageInGroup>
+        <UserMessage text="hi"></UserMessage>
+        <UserMessage text="hi"></UserMessage>
+        <OtherUserMessageInGroup text='hello' username='alireza' avatarSrc=''></OtherUserMessageInGroup>
+        <OtherUserMessageInGroup text='hello' username='alireza' avatarSrc=''></OtherUserMessageInGroup>
+        <OtherUserMessageInGroup text='hello' username='alireza' avatarSrc=''></OtherUserMessageInGroup>
+        <OtherUserMessageInGroup text='hello' username='alireza' avatarSrc=''></OtherUserMessageInGroup>
+
+        
+        </ChatPageGroup></div>}
     </MassengerLayout>
   );
 };
