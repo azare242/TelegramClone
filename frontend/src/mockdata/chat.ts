78aac513f035044081983d4f522ba6c7860d6531
azare242 <azare242@gmail.com>
2024-02-02 00:55:35 +0330

    feat ( front ) : chats

---
+++ frontend/src/mockdata/chat.ts
@@ -0,0 +1,40 @@
+import DOG1 from '../assets/dog1.jpeg'
+import DOG2 from '../assets/dog2.webp'
+import DOG3 from '../assets/dog3.jpg'
+
+const data = [
+    {
+        from: "sajad",
+        image: DOG1,
+        lastMessage: "nmidonam",
+        time: "7:30 AM",
+    },
+    {
+        from: "momod",
+        image: DOG2,
+        lastMessage: "dash bia berim",
+        time: "8:30 AM",
+    },
+    {
+        from: "ahora",
+        image: DOG3,
+        lastMessage: "haaaaa",
+        time: "9:00 AM",
+    },
+    {
+        from: "sadegh",
+        image: DOG1,
+        lastMessage: "berim r6?",
+        time: "10:00 AM",
+    },
+    {
+        from: "alireza",
+        image: DOG1,
+        lastMessage: "bye",
+        time: "10:00 AM",
+    },
+
+]
+
+
+export default data;
---
+++ frontend/src/mockdata/contact.ts
@@ -0,0 +1,14 @@
+import DOG1 from '../assets/dog1.jpeg';
+import DOG2 from '../assets/dog2.webp';
+import DOG3 from '../assets/dog3.jpg';
+
+const data = [
+  { username: 'sajad', avatarSrc: DOG1, phone: '' },
+  { username: 'momod', avatarSrc: DOG2, phone: '' },
+  { username: 'ahora', avatarSrc: DOG3, phone: '' },
+  { username: 'sadegh', avatarSrc: DOG1, phone: '' },
+];
+
+
+
+export default data;
