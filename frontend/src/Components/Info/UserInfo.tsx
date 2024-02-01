d4c174a24e62c22d4f31eb8b32df985f426a24e2
azare242 <azare242@gmail.com>
2024-02-01 18:42:38 +0330

    feat(front): user info and group info

--- frontend/src/Components/Info/UserInfo.tsx
+++ frontend/src/Components/Info/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { TextField, IconButton, Avatar, Typography, Button } from '@mui/material';
+import { TextField, IconButton, Avatar, Typography, Button, Badge } from '@mui/material';
 import React from 'react';
 import ContactList from '../Contacts/ContactList';
 import { Search } from '@mui/icons-material';
@@ -6,80 +6,29 @@ import { Search } from '@mui/icons-material';
 const UserInfo = () => {
   return (
     <div className='flex flex-col rounded-2xl border-blue-500 bg-white/70 backdrop-blur-sm mt-[100px]'>
-      <div>
-      <Button color="primary">
-        add member
-      </Button>
-      <Button color="error">
-        Delete Group
-      </Button>
-      <Button color="error">
-        Leave
-      </Button>
-      </div>
-      <div className='flex flex-row items-center justify-center p-3 gap-3'>
+
+      <div className='flex flex-col items-center justify-center p-3 gap-3'>
+        <Badge variant='dot' color="primary">
         <Avatar></Avatar>
-        <Typography>Group Name</Typography>
-      </div>
-      <div className='flex items-center justify-between p-4'>
-        <TextField label={'Search'} color={`primary`} />
-        <IconButton>
-          <Search />
-        </IconButton>
+        </Badge>
+        <Typography>last seen</Typography>
+
+        <Typography className='flex flex-col border-2 p-3 border-blue-500 rounded-lg w-full items-center justify-center'>Username</Typography>
+        <Typography className='flex flex-col border-2 p-3 border-blue-500 rounded-lg w-full items-center justify-center'>Name</Typography>
+
+        <Typography className='flex flex-col border-2 p-3 border-blue-500 rounded-lg w-full items-center justify-center' >phone number</Typography>
+
       </div>
 
-      <ContactList
-        contactItems={[
-          {
-            id: 1,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 2,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 3,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 4,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 5,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 6,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 7,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-          {
-            id: 8,
-            username: 'Alireza',
-            avatarSrc: '',
-            phone: '+989123456789',
-          },
-        ]}
-      />
+      <div>
+      <Button color="primary">
+        add contact
+      </Button>
+      <Button color="error">
+        Delete chat
+      </Button>
+
+      </div>
     </div>
   );
 };
